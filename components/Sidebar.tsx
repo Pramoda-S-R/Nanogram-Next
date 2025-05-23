0f94d66bb16a9784898e3a83ab395d2b3665583e
Pramoda-S-R <pramoda9.2.2004@gmail.com>
2025-05-23 10:33:22 +0530

    prog: routes grouped

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className='w-52'>
       Sidebar
-      <nav>
-        
+      <nav className='flex flex-col gap-5'>
+        <Link href='/community'>FYP</Link>
+        <Link href='/explore'>Explore</Link>
+        <Link href='/all-users'>People</Link>
+        <Link href='/saved'>Saved</Link>
+        <Link href='/create-post'>Create Post</Link>
+        <Link href='/messages'>Messages</Link>
       </nav>
     </div>
   )
