fe0aef5167617c33d9a783990c0beaf6daadaae5
Pramoda-S-R <pramoda9.2.2004@gmail.com>
2025-07-06 20:33:11 +0530

    prog: need to work out contact fetching logic

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -6,7 +6,16 @@
 // export const GET = withAuth(async (req: NextRequest) => {
 //   try {
 //     const searchParams = req.nextUrl.searchParams;
-//     const userId = searchParams.get("userId");
+//     const senderId = searchParams.get("senderId");
+//     const conversationId = searchParams.get("id");
+//     const receiverId = searchParams.get("receiverId");
+//     const seen = searchParams.get("seen") === "true";
+//     const sort = searchParams.get("sort") || "updatedAt";
+//     const order = parseInt(searchParams.get("order") || "-1");
+//     const limit = parseInt(searchParams.get("limit") || "20");
+//     const skip = parseInt(searchParams.get("skip") || "0");
+//     const query = {}
+    
 //   } catch (error: any) {
 //     return NextResponse.json({ error: error.message }, { status: 500 });
 //   }
